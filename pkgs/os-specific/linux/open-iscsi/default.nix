2c4db261f85cec01a90ad788f5060f2fa2cb57d7
Francesco Zanini <francesco@zanini.me>
2018-11-19 13:34:30 +0100

    Update openscsi

--- pkgs/os-specific/linux/open-iscsi/default.nix
+++ pkgs/os-specific/linux/open-iscsi/default.nix
@@ -1,26 +1,26 @@
-{ stdenv, fetchFromGitHub, automake, autoconf, libtool, gettext, utillinux, openisns, openssl, kmod }:
+{ stdenv, fetchFromGitHub, automake, autoconf, libtool, gettext, utillinux, openisns, openssl, kmod, perl, systemd, pkgconf }:
 stdenv.mkDerivation rec {
   name = "open-iscsi-${version}";
-  version = "2.0-873-${stdenv.lib.substring 0 7 src.rev}";
+  version = "2.0-877-${stdenv.lib.substring 0 7 src.rev}";
 
-  buildInputs = [ automake autoconf libtool gettext utillinux openisns.lib openssl kmod ];
-  
+  buildInputs = [ automake autoconf libtool gettext utillinux openisns.lib openssl kmod perl systemd pkgconf ];
+
   src = fetchFromGitHub {
     owner = "open-iscsi";
     repo = "open-iscsi";
-    rev = "4c1f2d90ef1c73e33d9f1e4ae9c206ffe015a8f9";
-    sha256 = "0h030zk4zih3l8z5662b3kcifdxlakbwwkz1afb7yf0cicds7va8";
+    rev = "0ec3a81158ef32da0a2866f8e2cc2ab02f2e3662";
+    sha256 = "0icwgcgldildb3blib850f7kll1knyxdylk2ic33rcddr60rxib2";
   };
-  
+
   DESTDIR = "$(out)";
-  
+
   NIX_LDFLAGS = "-lkmod";
   NIX_CFLAGS_COMPILE = "-DUSE_KMOD";
 
   preConfigure = ''
     sed -i 's|/usr|/|' Makefile
   '';
-  
+
   postInstall = ''
     cp usr/iscsistart $out/sbin/
     $out/sbin/iscsistart -v
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "A high performance, transport independent, multi-platform implementation of RFC3720";
     license = licenses.gpl2Plus;
-    homepage = http://www.open-iscsi.com;
+    homepage = https://www.open-iscsi.com;
     platforms = platforms.linux;
     maintainers = with maintainers; [ cleverca22 ];
   };
